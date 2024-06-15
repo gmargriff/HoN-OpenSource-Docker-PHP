94060a8bbe3d1c598970bd9e6eec76c3fa6a0c22
Gustavo Barbosa <bveddu@gmail.com>
2024-06-15 01:55:43 -0300

    get_products first call

--- public/client_requester.php
+++ public/client_requester.php
@@ -66,16 +66,8 @@ if (isset($_REQUEST['f'])) {
         $response = serialize($user_stats);
         echo $response;
     } else if ($_REQUEST['f'] == "get_products") {
-        $response = file_get_contents("public_docs/store_products_model.json");
+        $response = file_get_contents("public_docs/get_products.json");
         $response = json_decode($response, true);
-        $response["timestamp"] = time();
-        $response["requestHostTime"] = $_REQUEST["hostTime"];
-        $owned = explode("|", $response["productAlreadyOwned"]);
-        $response["productAlreadyOwned"] = "";
-        foreach ($owned as $o) {
-            $response["productAlreadyOwned"] .= 1 . "|";
-        }
-        $response["productAlreadyOwned"] = trim($response["productAlreadyOwned"], "|");
         $response = serialize($response);
         echo $response;
     } else if ($_REQUEST['f'] == "pre_auth") {
