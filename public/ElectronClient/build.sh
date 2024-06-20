9bd228d6db975d56efca12522c522fa3bda5124b
Gustavo Barbosa <bveddu@gmail.com>
2024-06-20 20:29:15 -0300

    fix permission for output binaries

--- public/ElectronClient/build.sh
+++ public/ElectronClient/build.sh
@@ -16,7 +16,18 @@ echo "-------------------------------------------"
 npm run make -- --platform darwin
 
 echo "-------------------------------------------"
-echo "Done building Linux binares"
+echo " Start building Linux binaries"
 echo "-------------------------------------------"
 npm run make -- --platform linux
+
+echo "-------------------------------------------"
+echo " Fixing permissions for output files"
+echo "-------------------------------------------"
+chmod 777 -R /app/out/
+
+echo ""
+echo ""
+echo "-------------------------------------------"
+echo " Done building binaries"
+echo "-------------------------------------------"
 exit 0
